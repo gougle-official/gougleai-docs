33124178d43f55033206b269e6b51b49761f95bc
foxypiratecove37350 <foxyFnaf37@chromium.org>
2024-03-09 16:54:34 +0100

    Added extension `sphinx.ext.autosectionlabel` to ReadTheDocs's Sphinx.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,8 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+autosectionlabel_prefix_document = True
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
